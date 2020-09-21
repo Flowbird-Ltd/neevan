2f0347944ca44247a5cae4dd2cb832ce806c85f9
Flowbird Ltd <59435868+Flowbird-Ltd@users.noreply.github.com>
2020-09-21 09:12:36 +0100

    Add files via upload

---
+++ send_mail_button/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+{
+    'name' : 'test sen button',
+    'version': '0.1',
+    'summary': 'Module customization',
+    'category': 'Custom Development',
+    'author': 'Odoo Inc',
+    'description':
+        """
+Test
+==============
+
+send email button
+        """,
+    'data': ['views/crm_lead_view.xml'],
+    'depends': ['crm'],
+}
